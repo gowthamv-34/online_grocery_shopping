262155c3142df4046c0fe68336b862bfabe3d0db
gowthamv-34 <111886032+gowthamv-34@users.noreply.github.com>
2023-05-13 21:21:51 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:kandanstores.online2020@gmail.com">kandanstores.online@gmail.com</a>
+         <a href="mailto:malarvizhik@gmail.com">malarvizhik@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">perundurai,Erode-638052</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:6381234667">6381234667</a>
+         <a href="tel:6379306614">6379306614</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>kandanstores.online</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
